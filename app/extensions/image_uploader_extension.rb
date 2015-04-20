1b09c803713f3a9a96209d00fd674a6165e14f7a
John Koht <john@kohactive.com>
2015-04-19 20:24:31 -0500

    image uploader sizes

--- app/extensions/image_uploader_extension.rb
+++ app/extensions/image_uploader_extension.rb
@@ -1,12 +1,19 @@
 ImageUploader.class_eval do
 
+  version :small do
+    process :resize_to_fill => [305, 175]
+  end
+
   version :medium do
-    process :resize_to_fill => [350, 350]
+    process :resize_to_fill => [400, 265]
   end
 
   version :large do
-    process :resize_to_fill => [640, 480]
+    process :resize_to_fill => [660, 420]
   end
 
+  version :banner do
+    process :resize_to_fill => [960, 620]
+  end
 
 end
